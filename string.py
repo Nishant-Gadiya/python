f20ddc4e3ce54a8998f9b68b22dac197af2ca9ad
Charan-S-K <www.c.tej2000@gmail.com>
2026-07-13 18:51:56 +0530

    String Operation 1

---
+++ string.py
@@ -0,0 +1,2 @@
+str=input("Enter the String: ")
+print(f"\nNumber of characters: {len(str)}\n Number of words: {len(str.split())}\n The sentence in uppercase: {str.upper()}\nThe sentence in lowercase: {str.lower()}")
